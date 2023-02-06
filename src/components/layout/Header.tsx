71ae51e449088b80e5c9daea667f138fd6e8924c
nocturne9no1 <nocturne9no1@gmail.com>
2023-02-07 00:22:40 +0900

    feat: header base

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,20 @@
 import { NavLink } from "react-router-dom";
+import { ReactComponent as IcoHamburger } from "@icons/ico_hamburger.svg";
+import { ReactElement } from "react";
 
-function Header() {
+function Header(): ReactElement {
   return (
     <div className="header-wrap">
       <div className="header">
+        <button type="button" aria-label="SNB button">
+          <IcoHamburger />
+        </button>
         <h1 className="logo">
           <NavLink to="/">logo</NavLink>
         </h1>
+        <button type="button" aria-label="language button" className="lang-btn">
+          ko
+        </button>
       </div>
     </div>
   );
