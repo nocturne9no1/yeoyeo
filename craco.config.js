0534e7ab83c28aaa5ca12a690441b43562c14b69
nocturne9no1 <nocturne9no1@gmail.com>
2023-02-06 11:56:28 +0900

    feat: change default port to 3005

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,9 @@ const { CracoAliasPlugin } = require("react-app-alias");
 const options = {};
 
 module.exports = {
+  devServer: {
+    port: 3005,
+  },
   plugins: [
     {
       plugin: CracoAliasPlugin,
