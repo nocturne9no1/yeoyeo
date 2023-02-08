33e0cac5cef14329d0163f02f8f5e11931d55ea2
alexkwonpro <alexkwonpro@gmail.com>
2023-02-09 00:58:18 +0900

    feat: room before swiper

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,11 +1,26 @@
 // import { useState, useEffect } from "react";
+// import outsideImg from "@temp/yeoyeo_outside.jpg";
+import cn from "classnames";
 
 function Room() {
   // const [cnt, setCnt] = useState<number>(0);
 
   return (
     <div>
-      <span className="my-span">room</span>
+      {/* 배너 */}
+      <div className={cn("banner-img-wrap")}>{/* <img src={outsideImg} alt="yeoyeo_outside" /> */}</div>
+
+      <div className={cn("room-wrap")}>
+        {/* 공간도 */}
+        <div>
+          <div>단면도</div>
+        </div>
+
+        {/* swiper */}
+        <div>
+          <div>스와이퍼</div>
+        </div>
+      </div>
     </div>
   );
 }
