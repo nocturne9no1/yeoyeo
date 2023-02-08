c74c9c99b9a7b14bf8493339a23a8b8d430f7eeb
edkim3275 <edkim3275@gmail.com>
2023-02-09 00:24:01 +0900

    feat : intro page layout

---
+++ src/assets/index.js
@@ -0,0 +1,1 @@
+export { default as IntroBackgroundImage } from "./images/intro/hanok.png";
--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,5 +1,51 @@
+import { IntroContents } from "src/constants/contents";
+
 function Intro() {
-  return <div>intro</div>;
+  const { body } = IntroContents;
+
+  const poemGenerator = (poem: string, paragraphSpacing?: number[]) => {
+    const result = poem.split("\n");
+    if (paragraphSpacing) {
+      const sortedArr = paragraphSpacing?.sort();
+      for (let i = 0; i < sortedArr?.length; i += 1) {
+        result.splice(paragraphSpacing[i] + i, 0, "");
+      }
+    }
+    return result;
+  };
+  return (
+    <div className="intro-wrap">
+      <article className="contents-section">
+        <div className="top">
+          <p>Intro page</p>
+          <p>Content top</p>
+        </div>
+        <div className="body">
+          <p>Content body</p>
+          <div className="poem">
+            {poemGenerator(body.poem, [4, 9, 13, 17]).map((sentence) => {
+              if (!sentence) {
+                return <br />;
+              }
+              return (
+                <p>
+                  <span>{sentence}</span>
+                </p>
+              );
+            })}
+          </div>
+        </div>
+        <div className="bottom">
+          <button className="nav-resrve-btn" type="button">
+            예약하기
+          </button>
+        </div>
+        {/* <div className="side">
+
+        </div> */}
+      </article>
+    </div>
+  );
 }
 
 export default Intro;
