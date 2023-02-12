7cc5fed66707027a97d6b0e3a59f1c2bbde7c942
alexkwonpro <alexkwonpro@gmail.com>
2023-02-13 08:51:00 +0900

    feat: room layout css

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,20 +1,34 @@
 // import { useState, useEffect } from "react";
-// import outsideImg from "@temp/yeoyeo_outside.jpg";
+import outsideImg from "@temp/yeoyeo_outside.jpg";
+import floorPlan from "@temp/room_spaces.jpg";
 import cn from "classnames";
 
 function Room() {
   // const [cnt, setCnt] = useState<number>(0);
 
   return (
-    <div>
+    <div className={cn("room-wrap")}>
       {/* 배너 */}
-      <div className={cn("banner-img-wrap")}>{/* <img src={outsideImg} alt="yeoyeo_outside" /> */}</div>
+      <div className={cn("banner-img-wrap")}>
+        <img src={outsideImg} alt="yeoyeo-outside" />
+      </div>
 
-      <div className={cn("room-wrap")}>
-        {/* 공간도 */}
-        <div>
-          <div>단면도</div>
+      <div className={cn("room-inner")}>
+        {/* 공백 및 구분선 */}
+        <div className={cn("room-padding")} />
+        <div className={cn("divider")}>
+          <div className={cn("divider-line")} />
         </div>
+
+        {/* 공간도 */}
+        <div className={cn("floor-plan")}>
+          <img src={floorPlan} alt="floor-plan" />
+        </div>
+
+        <div className={cn("divider")}>
+          <div className={cn("divider-line")} />
+        </div>
+        <div className={cn("room-padding")} />
 
         {/* swiper */}
         <div>
