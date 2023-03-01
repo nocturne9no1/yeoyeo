96a63303df81f39dfe82aefbf35816bdaffce836
alexkwonpro <alexkwonpro@gmail.com>
2023-03-02 00:44:22 +0900

    feat: typo fix key

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -20,7 +20,7 @@ function Reservation() {
     } = response;
 
     if (success) {
-      alert('결제 성공');
+      alert("결제 성공");
     } else {
       alert(`결제 실패: ${error_msg}`);
     }
@@ -29,27 +29,27 @@ function Reservation() {
     try {
       /* 1. 가맹점 식별하기 */
       const { IMP } = window;
-      IMP.init('imp28607423');
-  
+      IMP.init("imp28607423");
+
       /* 2. 결제 데이터 정의하기 */
       const data = {
         pg: "nice",
         // pg: "kakaopay"
         pay_method: "card",
-        merchant_uid: "2023030210test", // 고유 주문번호 (날짜+방)
+        merchant_uid: "20230302101test", // 고유 주문번호 (날짜+방)
         name: "여여 결제 테스트",
         amount: 10000, // 결제금액
         buyer_email: "toto9091@naver.com",
         buyer_name: "박정웅",
         buyer_tel: "010-1234-7777",
         buyer_addr: "서울특별시 강남구 신사동",
-        buyer_postcode: "01181"
-    }
-    
-    IMP.request_pay(data, callBack);
+        buyer_postcode: "01181",
+      };
+
+      IMP.request_pay(data, callBack);
     } catch (e) {
-      console.log(e)
-    } 
+      console.log(e);
+    }
   }
   return (
     <div className={cn("reservation-wrap")}>
