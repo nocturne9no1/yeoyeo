942ee0cafd876099878416f344a394b7c29184c2
alexkwonpro <alexkwonpro@gmail.com>
2023-03-01 22:10:47 +0900

    feat: room & footer basic complete

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Layout() {
   return (
     <div className="page-wrap">
       <Header />
       <Outlet />
-      {/* TODO: Insert Footer */}
+      <Footer />
     </div>
   );
 }
