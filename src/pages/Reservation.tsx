c94625eaff95ed89a6eb3ed0dab3f2d9c44546b4
nocturne9no1 <nocturne9no1@gmail.com>
2023-03-01 22:45:05 +0900

    feat: td component 분리

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -8,30 +8,15 @@ import CustomerForm from "@components/reservation/CustomerForm";
 import ReservationSidebar from "@components/reservation/ReservationSidebar";
 
 function Reservation() {
-  // const [calendarData, setCalendarData] = useState<MonthRoomData[]>();
-
-  // useEffect(() => {
-  //   axios({
-  //     method: "get",
-  //     url: "http://3.35.98.5:8080/dateroom/2023/2",
-  //   }).then((res) => setCalendarData(res.data));
-  // }, []);
-
-  // const [startDate, setStartDate] = useState<Date | null>(null);
-  // const [endDate, setEndDate] = useState<Date | null>(null);
-  // const accommodationPeriod = ((Number(endDate) - Number(startDate)) / 1000 / 60 / 60 / 24 );
   const accommodationPeriod = (Number(new Date()) - Number(new Date())) / 1000 / 60 / 60 / 24;
-  // const [defaultFeePerDay, setDefaultFeePerDay] = useState<number | null>(null);
 
   return (
     <div className={cn("reservation-wrap")}>
       <div className={cn("reservation-inner")}>
         Reservation
-        {/* <DatePicker startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} setDefaultFeePerDay={setDefaultFeePerDay}/> */}
         <DatePicker />
-        <div style={{ display: "flex" }}>
+        <div className={cn("reservation-form-wrap")}>
           <CustomerForm />
-
           <ReservationSidebar
             startDate={new Date()}
             endDate={new Date()}
