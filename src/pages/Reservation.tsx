59e06cfbfcb49d6c85c58e7624ab70b135215241
edkim3275 <edkim3275@gmail.com>
2023-03-01 17:54:31 +0900

    feat : 예약정보 사이드바

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -4,6 +4,8 @@ import cn from "classnames";
 
 import DatePicker from "@components/reservation/date-picker/DatePicker";
 import CustomerForm from "@components/reservation/CustomerForm";
+// import { useState } from "react";
+import ReservationSidebar from "@components/reservation/ReservationSidebar";
 
 function Reservation() {
   // const [calendarData, setCalendarData] = useState<MonthRoomData[]>();
@@ -13,14 +15,30 @@ function Reservation() {
   //     method: "get",
   //     url: "http://3.35.98.5:8080/dateroom/2023/2",
   //   }).then((res) => setCalendarData(res.data));
-  // }, []);
+  // }, []); 
+
+  // const [startDate, setStartDate] = useState<Date | null>(null);
+  // const [endDate, setEndDate] = useState<Date | null>(null);
+  // const accommodationPeriod = ((Number(endDate) - Number(startDate)) / 1000 / 60 / 60 / 24 );
+  const accommodationPeriod = ((Number(new Date()) - Number(new Date())) / 1000 / 60 / 60 / 24 );
+  // const [defaultFeePerDay, setDefaultFeePerDay] = useState<number | null>(null);
 
   return (
     <div className={cn("reservation-wrap")}>
       <div className={cn("reservation-inner")}>
         Reservation
+        {/* <DatePicker startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} setDefaultFeePerDay={setDefaultFeePerDay}/> */}
         <DatePicker />
+        <div style={{display:'flex'}}>
         <CustomerForm />
+
+        <ReservationSidebar
+          startDate={new Date()}
+          endDate={new Date()}
+          accommodationPeriod={accommodationPeriod}
+          defaultFeePerDay={200}
+        />
+        </div>
       </div>
     </div>
   );
