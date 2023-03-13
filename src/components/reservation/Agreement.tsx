839d8111fe5768ddf26f52cf429b6dd8a64a4393
edkim3275 <edkim3275@gmail.com>
2023-03-14 02:55:00 +0900

    feat : 이용약관 layout

--- src/components/reservation/Agreement.tsx
+++ src/components/reservation/Agreement.tsx
@@ -14,6 +14,16 @@ function Agreement() {
           </div>
         </div>
       </div>
+      <div className={cn("agreement-section")}>
+        <div className={cn("agreement-title")}>
+          <span>▪️ 여여 이용약관(필수)</span>
+          <div className={cn("btn-wrap")}>
+            <button type="button" aria-label="menu close" onClick={() => console.log("helelo")}>
+              <ChevronDown />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
