e57331f6ef1c66e683e7d85d7a383dd30271df4f
nocturne9no1 <nocturne9no1@gmail.com>
2023-03-17 00:45:51 +0900

    feat: axios default url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import "@styles/common.scss";
+import axios from "axios";
 import Router from "./utils/Router";
+
+axios.defaults.baseURL = "https://yeoyeo.kr";
 
 function App() {
   return (
