12fd8a3a5e42ef2fbd50cfc2186d4b7383bbc141
chen.kim <chen.kim@kakaocorp.com>
2023-06-07 20:23:56 +0900

    fix: adminCalendar delete

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,10 @@
-import AdminCalendar from "@components/admin/AdminCalendar";
 import Table from "@components/admin/Table";
 
 function Admin() {
   return (
     <>
-      <AdminCalendar />
       <Table />
+      {/* <Table /> */}
     </>
   );
 }
